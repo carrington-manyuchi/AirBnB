c9a380ce2575ef4921ecc357da315b867bd2e959
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-05-23 20:53:15 +0200

    Initial Commit

---
+++ AirBnB/AirBnBApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnBApp.swift
+//  AirBnB
+//
+//  Created by Manyuchi, Carrington C on 2025/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AirBnBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
